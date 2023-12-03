ba9cf3942519aa87e583c7c89d10ddf49763cf10
Sobir Alimov <alimoff@Sobirs-Laptop.local>
2023-12-04 01:07:25 +0500

    a

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,9 +4,7 @@ import { authRouter } from "./authRoutes";
 
 const router = Router();
 
-const helloText:any = "All images => 'https://imageene-backend.up.railway.app'"
 router.use("/api", imageeneRouter);
 router.use("/api", authRouter);
-router.use('/',helloText)
 
 export {router};
