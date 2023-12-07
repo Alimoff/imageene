c8dde364e866ab3d497918e0b127ea439f2565ce
Sobir Alimov <alimoff@Sobirs-Laptop.local>
2023-12-07 23:55:05 +0500

    middleware edit

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -14,29 +14,11 @@ export const fileUpload  = (destination:any) =>multer({
   fileSize:20*1024*1024,
  },
  fileFilter:(req,file,cb) =>{
-  if(file.mimetype == "image/png" || file.mimetype == "image/jpeg" || file.mimetype == "image/jpg" || file.mimetype == "image/heic"){
+  // if(file.mimetype == "image/png" || file.mimetype == "image/jpeg" || file.mimetype == "image/jpg"){
     cb(null,true);
-  }else{
-    cb(null,false);
-    return cb(new Error("Only .png, .jpeg, .jpg, .heic formats allowed!"));
-  }
+  // }else{
+  //   cb(null,false);
+  //   return cb(new Error("Only .png, .jpeg, .jpg formats allowed!"));
+  // }
  }
 }).single('image');
-
-
-
-
-// const Storage = multer.diskStorage({
-//   destination: 'static',
-//   filename: (req, file, cb) => {
-//     // Assuming you have a 'name' field in the request body
-//     const imageName = req.body.name || 'defaultName'; // Provide a default name if 'name' is not available
-//     const filename = `${imageName}.jpg`;
-
-//     cb(null, filename);
-//   }
-// });
-
-// export const upload = multer({
-//   storage: Storage
-// }).single('image');
