5cede6b5a299ee5eec33bc5f084c215eddfed7e5
Sobir Alimov <alimoff@Sobirs-Laptop.local>
2023-12-08 01:03:21 +0500

    a

--- src/controller/imageene.ts
+++ src/controller/imageene.ts
@@ -1,6 +1,8 @@
 import {Request,Response} from "express";
 import { ImageModel } from "../database/imageModel";
 import path from "path";
+import fs from 'fs/promises';
+import mongoose from "mongoose";
 //Method GET
 //get all images
 export const getAllImages = async(req:Request,res:Response) => {
@@ -16,19 +18,19 @@ export const getOneImage = async(req:Request,res:Response) => {
     const {id} = req.params;
     try{
     const data = await ImageModel.findById({_id:id});
-    res.status(200).json({status:"200 ok", data});
     if(!data){
         res.status(404).json({error:"Image not found"});
     }else{
-        res.json(data);
-    }}catch(error){
-        res.json({message:error});
+    res.status(200).json({status:"200 ok", data});}
+  }catch(error){
+        res.status(501).json({message:error});
     }};
 //Method POST
 //Add new image
 export const createImage = async(req:Request,res:Response)=>{
   const imageName = req.params.imageName;
    const {name,userId} = req.body;
+
    let image = "";
    if(req.file) image = `${req.file.filename}`
    if (!imageName) {
@@ -52,45 +54,64 @@ export const createImage = async(req:Request,res:Response)=>{
 }
 //Method DELETE
 //Delete an image
-export const deleteOneImage = async (req:Request,res:Response) =>{
-    const {id} = req.params;
-    try{
-        const deleteImage = await ImageModel.findOneAndDelete({_id:id});
-        if(deleteImage){
-        await deleteImage.save()
 
-        return  res.status(200).json({status:"200 ok", message:"Image deleted successfully"});
-        }
-        else{
-          res.status(404).json({message: 'Image does not exist'});
-        }
-    }catch(error){
-        res.status(500).json({message:error})
-    }
-};
-//Method PUT
-//Update an image
-export const updateImage = async (req: Request, res: Response) => {
+
+export const deleteOneImage = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
   try {
-    const { id } = req.params;
-    const { name } = req.body;
+    // Convert id to ObjectId
+    const imageId = new mongoose.Types.ObjectId(id);
 
-    const updateImage = await ImageModel.findById({_id:id});
-    if (!updateImage) {
-      res.status(404).json({ message: 'Image not found' });
-      return;
+    // Find and delete the image document from MongoDB
+    const deleteImage = await ImageModel.findByIdAndDelete(imageId);
+
+    if (!deleteImage) {
+      return res.status(404).json({ message: 'Image does not exist' });
     }
-    // Update image name in the database
-    updateImage.name = name;
-    updateImage.image = `${req.file?.filename}`;
 
-    await updateImage.save();
+    // Get the file name from the document
+    const fileName = deleteImage.image;
 
-    res.json(updateImage);
+    // Construct the file path in the static folder
+    const filePath = path.join(__dirname, '../../static', fileName);
+
+    // Check if the file exists before attempting to delete
+    await fs.access(filePath);
+
+    // Delete the file from the static folder
+    await fs.unlink(filePath);
+
+    return res.status(200).json({ status: '200 ok', message: 'Image deleted successfully' });
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error' });
+    console.error('Error deleting image:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
 };
+
+//Method PUT
+//Update an image
+    export const updateImage = async (req: Request, res: Response) => {
+        try{
+        const { id } = req.params;
+        const { name } = req.body;
+    
+        const updateImage = await ImageModel.findByIdAndUpdate({_id:id});
+        if (!updateImage) {
+          res.status(404).json({ message: 'Image not found' });
+          return;
+        }
+
+        // Update image name in the database
+        updateImage.name = name;
+    
+        await updateImage.save();
+    
+        res.json(updateImage);
+  }catch(error){
+    res.status(500).json({message: "Internal Server Error "});
+  }}
+
 
   export const getImage = async (req:Request,res:Response) =>{
     try{
